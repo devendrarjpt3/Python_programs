47e6d065910e214ce4948e135876a3706ba4d1b8
Devendra Rajput <45932483+devendrarjpt3@users.noreply.github.com>
2025-04-02 15:35:27 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,34 @@
+w=5
+w%=3
+print(w)
+print("________________")
+v=50
+v//=11
+print(v)
+print("________________")
+print(x:=9)
+z = isinstance(x, int)
+print(z)
+x**=2
+print (f"square of 9 is {x}")
+print("________________")
+y=5  #101 in binary
+y &= 3 #and operator in binary(101 & 011 = 001)
+print(y)
+a=5
+a|=3
+print(a)  # or operator  in binary (101 || 011 = 111)
+b=5
+b^=3
+print(b) # XOR of (101 and 011= 110)
+c=5
+c>>=3
+print(c) # 3 left shift of binary (101 --->000)
+d=5
+d<<=3
+print (d) # 3 left shift of binary (101 ---> 101000)
+
+
+
+
+
